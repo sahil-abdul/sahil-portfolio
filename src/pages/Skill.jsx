3bc9a23c120f5985bda0cb54fa4f012d18fa535e
sahil-abdul <sahilabdul264@gmail.com>
2025-08-20 09:31:10 +0530

    Update Skill.jsx

--- src/pages/Skill.jsx
+++ src/pages/Skill.jsx
@@ -7,14 +7,14 @@ function Skill() {
   const program = ["Java","C++","C","Sql","Mongodb","JavaScript"]
   return (
     <>
-      <div id="skill" className="h-full w-full flex flex-col justify-evenly items-center flex-wrap bg-[#1d1d1f] mt-10 mb-10 overflow-x-hidden">
+      <div id="skill" className="h-full w-full flex flex-col justify-evenly items-center bg-[#1d1d1f] mt-10 mb-10 overflow-x-hidden">
         <div className="flex flex-col justify-between items-center " data-aos="zoom-out-down">
           <h1 className="text-4xl md:text-5xl font-bold tracking-wider">
             My Skills
           </h1>
           <div className="w-1/2 bg-[#9f3877] h-2 mt-4 rounded"></div>
         </div>
-        <div className="flex w-full justify-around gap-10 flex-col md:flex-row">
+        <div className="flex w-full justify-around gap-10 flex-col md:flex-row flex-wrap">
 
         <SkillBox skills = {fullStack} head= {"Full Stack Development"} ><WebhookIcon/> </SkillBox>
         <SkillBox skills = {program} head= {"Programming Skills"}><PsychologyIcon/> </SkillBox>
@@ -24,4 +24,5 @@ function Skill() {
   );
 }
 export default Skill;
+
 
